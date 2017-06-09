a08331886131dc1741cd07b49a52ba30f32d0921
Dilip Shukla <dilip@Dilip-Shuckla-MBP.local>
2017-06-09 11:47:45 +0200

    add babel-plugin-syntax-dynamic-import

--- jspm.config.js
+++ jspm.config.js
@@ -1,35 +1,40 @@
 SystemJS.config({
-  paths: {
-    "npm:": "jspm_packages/npm/",
-    "jspm-stack/": "src/"
-  },
-  browserConfig: {
-    "baseURL": "/"
-  },
-  devConfig: {
-    "map": {
-      "plugin-babel": "npm:systemjs-plugin-babel@0.0.21"
-    }
-  },
-  transpiler: "plugin-babel",
-  packages: {
-    "jspm-stack": {
-      "main": "index.js",
-      "meta": {
-        "*.js": {
-          "loader": "plugin-babel"
-        }
-      },
-      "format": "esm"
-    }
-  }
+	paths: {
+		'npm:': 'jspm_packages/npm/',
+		'jspm-stack/': 'src/'
+	},
+	browserConfig: {
+		baseURL: '/'
+	},
+	devConfig: {
+		map: {
+			'plugin-babel': 'npm:systemjs-plugin-babel@0.0.21',
+			'babel-plugin-syntax-dynamic-import':
+				'npm:babel-plugin-syntax-dynamic-import@6.18.0'
+		}
+	},
+	transpiler: 'plugin-babel',
+	packages: {
+		'jspm-stack': {
+			main: 'index.js',
+			meta: {
+				'*.js': {
+					loader: 'plugin-babel',
+					babelOptions: {
+						stage1: true,
+						plugins: [
+							'babel-plugin-syntax-dynamic-import'
+						]
+					}
+				}
+			},
+			format: 'esm'
+		}
+	}
 });
 
 SystemJS.config({
-  packageConfigPaths: [
-    "npm:@*/*.json",
-    "npm:*.json"
-  ],
-  map: {},
-  packages: {}
+	packageConfigPaths: ['npm:@*/*.json', 'npm:*.json'],
+	map: {},
+	packages: {}
 });
